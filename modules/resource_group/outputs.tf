d5d63dfe52c7b18d667900d35d60beef5edd8243
Maruthi <gmaruthishankar1@gmail.com>
2024-09-24 15:50:27 -0400

    version_8_outputs

--- modules/resource_group/outputs.tf
+++ modules/resource_group/outputs.tf
@@ -1,1 +1,7 @@
-output "id" { value = azurerm_resource_group.this.id }
+output "name" {
+  value = azurerm_resource_group.example.name
+}
+
+output "location" {
+  value = azurerm_resource_group.example.location
+}
