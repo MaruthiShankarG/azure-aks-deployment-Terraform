b96bd016394a4795a9c950880914434ded4307e0
Maruthi <gmaruthishankar1@gmail.com>
2024-09-24 16:04:45 -0400

    version_12_Monitoring

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -2,22 +2,21 @@ resource "azurerm_monitor_diagnostic_setting" "this" {
   name               = "example-diagnostic-setting"
   target_resource_id = var.target_resource_id
 
-  log {
-    category = "kube-apiserver"
-    retention_policy {
-      enabled = false
-      days    = 0
-    }
-  }
-
-  metric {
-    category = "AllMetrics"
-    retention_policy {
-      enabled = false
-      days    = 0
-    }
-  }
-
-  # Assuming you're sending diagnostics to a Log Analytics Workspace
   log_analytics_workspace_id = var.workspace_id
+
+  logs {
+    category = "kube-apiserver"  # Example log category
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
+  }
+
+  metrics {
+    category = "AllMetrics"  # Example metrics category
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
+  }
 }
--- modules/monitoring/variables.tf
+++ modules/monitoring/variables.tf
@@ -1,3 +1,9 @@
-variable "name" { type = string }
-variable "target_resource_id" { type = string }
-variable "workspace_id" { type = string }
+variable "workspace_id" {
+  description = "The ID of the Log Analytics Workspace"
+  type        = string
+}
+
+variable "target_resource_id" {
+  description = "The ID of the target resource for diagnostics"
+  type        = string
+}
